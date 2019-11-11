0870cf41315125e2a113b2ca736f334a540d1589
chiq2045 <charles.oroko@outlook.com>
2019-11-11 00:43:56 +0000

    add routing to backend for homepage

--- onum.js
+++ onum.js
@@ -1,11 +1,18 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(`${__dirname}/.env`) });
 
 const Koa = require('koa');
+const logger = require('koa-logger');
+const koaBody = require('koa-body');
+const notFound = require('./backend/404.js');
 
 const onum = new Koa();
+const home = require('./backend/routes/home.js');
+onum.use(logger()).use(koaBody());
+onum.use(home.routes());
 
 onum.use((ctx, next) => {
-  ctx.body = 'Welcome to onum!';
+  notFound(ctx);
 });
 
-onum.listen(process.env.ONUM_PORT);
+onum.listen(process.env.PORT);
